c23d458488d05ff8267319cfdc1bbf77eebddf3b
brmdm <60578980+brmdm@users.noreply.github.com>
2021-01-06 12:45:43 +0200

    Init commit

---
+++ src/main/java/com/ua/knuca/committee/Application.java
@@ -0,0 +1,13 @@
+package com.ua.knuca.committee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ua/knuca/committee/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.knuca.committee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
