187b303bca5cf92cc221a17dfb66d34e20ba09ba
brmdm <60578980+brmdm@users.noreply.github.com>
2021-01-09 21:47:05 +0200

    MVC and Security Config files

--- src/main/java/com/ua/knuca/committee/service/impl/UserServiceImpl.java
+++ src/main/java/com/ua/knuca/committee/service/impl/UserServiceImpl.java
@@ -3,10 +3,17 @@ package com.ua.knuca.committee.service.impl;
 import com.ua.knuca.committee.repository.UserRepository;
 import com.ua.knuca.committee.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
